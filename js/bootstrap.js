31e06ea061fe4e7763f6d153e63c03d07c51eaf9
helenamsilva10-cpu <helena.m.silva10@aluno.senai.br>
2026-04-10 09:55:46 -0300

    Correções

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,21 +1,20 @@
-const dataCopa = new Date("June 11, 2026 00:00:00").getTime();
-
+  const dataCopa = new Date("June 11, 2026 00:00:00").getTime();
   const contador = setInterval(() => {
     const agora = new Date().getTime();
     const diferenca = dataCopa - agora;
-
+    
     const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
     const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
     const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
     const segundos = Math.floor((diferenca / 1000) % 60);
-
-    document.getElementById("dias").innerText = dias;
-    document.getElementById("horas").innerText = horas;
-    document.getElementById("minutos").innerText = minutos;
-    document.getElementById("segundos").innerText = segundos;
-
+    
+    document.getElementById("dias").innerText = dias < 10 ? '0' + dias : dias;
+    document.getElementById("horas").innerText = horas < 10 ? '0' + horas : horas;
+    document.getElementById("minutos").innerText = minutos < 10 ? '0' + minutos : minutos;
+    document.getElementById("segundos").innerText = segundos < 10 ? '0' + segundos : segundos;
+    
     if (diferenca < 0) {
       clearInterval(contador);
-      document.querySelector(".contador").innerHTML = "A Copa começou!";
+      document.querySelector(".contador").innerHTML = "<h2 style='color:#ffc107;'>A Copa começou!</h2>";
     }
   }, 1000);
